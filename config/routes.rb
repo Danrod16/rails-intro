a0b55cdae86071c727f7824cf614f70c0a52dd53
Daniel Rodriguez <daniel.rod93@gmail.com>
2022-08-15 17:43:03 +0800

    feat: added airbnb landing page without database, only JSON

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'flats/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # VERB '/path', to: 'controller_name#action'
+  get '/flats', to: 'flats#index', as: :flats
 end
